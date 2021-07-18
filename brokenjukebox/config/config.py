6f9598698f1790956e7adf51cb1ad66eefe8de20
arbitraryrw <nickcucakovic@gmail.com>
2021-07-18 22:42:58 +0100

    Config file loading logic with common config vars added to Config interface

---
+++ brokenjukebox/config/config.py
@@ -0,0 +1,34 @@
+import os
+from brokenjukebox.util.util import Util
+
+
+class Config:
+    ROOT_DIR=None
+    CONFIG_FILE = None
+    VERBOSE_MODE=None
+
+    TOKEN=None
+
+    VERSION = "0.1"
+    BANNER = f"""
+broken-jukebox v{VERSION}
+    """
+
+    def __init__(self):
+        self.read_json_config()
+
+    def __del__(self):
+        pass
+
+    def read_json_config(self):
+        Config.CONFIG_FILE = os.path.join(
+            self.ROOT_DIR,
+            "brokenjukebox/config/broken_jukebox_config.json"
+        )
+
+        deserialised_json_config = Util.deserialise_json_file(Config.CONFIG_FILE)
+
+        Config.TOKEN = deserialised_json_config.get('Token', None)
+
+        if Config.TOKEN is None:
+            exit("ERROR - unable to get TOKEN from config")
