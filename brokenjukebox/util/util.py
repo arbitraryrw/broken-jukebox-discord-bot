82a936f462fb8d99df0f8562b8d4dcb6db2648e6
arbitraryrw <nickcucakovic@gmail.com>
2021-07-24 11:12:07 +0100

    Common utility class updated to include additional useful helper functions

--- brokenjukebox/util/util.py
+++ brokenjukebox/util/util.py
@@ -1,6 +1,5 @@
-import os
 import json
-
+import os
 
 class Util:
 
@@ -8,13 +7,29 @@ class Util:
         pass
 
     @staticmethod
-    def deserialise_json_file(file_path: str) -> dict:
-        data = dict()
+    def deserialise_json_file(json_file_path: str) -> dict:
+        # Weak file check, simply check file extension is json..
+        if json_file_path[-5:] != ".json":
+            return None
 
-        if not os.path.isfile(file_path):
-            return data
+        if not os.path.isfile(json_file_path):
+            return None
 
-        with open(file_path) as f:
-                data = json.load(f)
-
+        with open(json_file_path) as file:
+            data = json.load(file)
+        
         return data
+
+    @staticmethod
+    def get_audio_files_in_dir(directory_path: str) -> list:
+        file_matches = list()
+
+        for (dir_path, dir_name, file_names) in os.walk(directory_path):
+            # file_matches = [os.path.join(dir_path, f) for f in file_names if f[-4:] == ".mp3"]
+            file_matches = [os.path.join(dir_path, f) for f in file_names if f  == "bestie.mp3"]
+        
+        return file_matches
+
+    @staticmethod
+    def pretty_format_json(json_string: str) -> str:
+        return json.dumps(json_string, indent=4, sort_keys=True)
