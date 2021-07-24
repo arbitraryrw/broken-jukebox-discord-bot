a07662cfc862a0cdf63f884a7f70f9b9909199fc
arbitraryrw <nickcucakovic@gmail.com>
2021-07-24 11:09:43 +0100

    Core logic added to orchestrate internals

--- brokenjukebox/core/core.py
+++ brokenjukebox/core/core.py
@@ -1,10 +1,23 @@
+import discord
 from brokenjukebox.config.config import Config
+from brokenjukebox.core.custom_discord_bot import CustomDiscordBot
+from brokenjukebox.core.cogs.command_error_handler import CommandErrorHandler
+from brokenjukebox.core.cogs.broken_jukebox import BrokenJukebox
+
+
 
 class Core:
-
     def __init__(self):
         # Initialise an instance of the Config class to run initialisation functions
         Config()
 
+
     def start(self):
-        print("Starting..")
+        print("[INFO] Starting discord bot..")
+
+        bot = CustomDiscordBot(command_prefix='!')
+
+        bot.add_cog(BrokenJukebox(bot))
+        bot.add_cog(CommandErrorHandler(bot))
+
+        bot.run(Config.TOKEN)
