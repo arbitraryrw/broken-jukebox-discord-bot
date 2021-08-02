cbda0f05f08be53a8178511ee0eda2515ae4dfde
arbitraryrw <nickcucakovic@gmail.com>
2021-08-02 22:10:22 +0100

    Config interface updated to support environment variables for passing in discord auth tokens

--- brokenjukebox/config/config.py
+++ brokenjukebox/config/config.py
@@ -18,10 +18,26 @@ broken-jukebox v{VERSION}
     """
 
     def __init__(self):
-        self.read_json_config()
+        if self.read_token_environment_variable() is not None:
+            print("[INFO] Getting discord token from DISCORD_TOKEN env variable")
+            Config.TOKEN = self.read_token_environment_variable()
+        else:
+            print("[INFO] Getting discord token from config file")
+            Config.TOKEN = self.read_json_config()
 
+        if Config.TOKEN is None:
+            exit("ERROR - unable to get TOKEN from config")
+        
     def __del__(self):
         pass
+
+    def read_token_environment_variable(self):
+        env_token = os.environ.get('DISCORD_TOKEN', None)
+
+        if env_token is not None and len(env_token) > 10:
+            return env_token
+
+        return None
 
     def read_json_config(self):
         Config.CONFIG_FILE = os.path.join(
@@ -31,7 +47,4 @@ broken-jukebox v{VERSION}
 
         deserialised_json_config = Util.deserialise_json_file(Config.CONFIG_FILE)
 
-        Config.TOKEN = deserialised_json_config.get('Token', None)
-
-        if Config.TOKEN is None:
-            exit("ERROR - unable to get TOKEN from config")
+        return deserialised_json_config.get('Token', None)
