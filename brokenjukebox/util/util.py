231eb895c4409e2e1d73ea02b69fca4fe9ea71a2
arbitraryrw <nickcucakovic@gmail.com>
2021-08-03 22:01:11 +0100

    Creating helper functions for reading and writing json files for consistency

--- brokenjukebox/util/util.py
+++ brokenjukebox/util/util.py
@@ -25,11 +25,36 @@ class Util:
         file_matches = list()
 
         for (dir_path, dir_name, file_names) in os.walk(directory_path):
-            # file_matches = [os.path.join(dir_path, f) for f in file_names if f[-4:] == ".mp3"]
-            file_matches = [os.path.join(dir_path, f) for f in file_names if f  == "bestie.mp3"]
+            file_matches = [os.path.join(dir_path, f) for f in file_names if f[-4:] == ".mp3"]
         
         return file_matches
 
     @staticmethod
     def pretty_format_json(json_string: str) -> str:
         return json.dumps(json_string, indent=4, sort_keys=True)
+    
+    @staticmethod
+    def write_json_to_file(file_path: str, data: str) -> None: 
+        # Attempt to remove the file
+        try:
+            os.remove(file_path)
+        except OSError as e:
+            pass
+
+        with open(file_path, 'w') as file:
+            file.write(json.dumps(data))
+
+    @staticmethod
+    def read_json_from_file(file_path: str) -> dict: 
+        if not os.path.isfile(file_path):
+            return None
+
+        json_data = None
+
+        with open(file_path, 'r') as file:
+            try:
+                json_data = json.loads(file.read())
+            except json.decoder.JSONDecodeError:
+                print(f"[WARNING] Failed to decode json from {file_path}")
+        
+        return json_data
