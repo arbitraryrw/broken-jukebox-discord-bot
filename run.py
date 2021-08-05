8edb2a4a4a10b978d2512104d7da81af2d7d2dac
arbitraryrw <nickcucakovic@gmail.com>
2021-08-05 21:31:51 +0100

    Updating main entrypoint to set catalogue path in Config class for re-use

--- run.py
+++ run.py
@@ -4,10 +4,10 @@ from brokenjukebox.config.config import Config
 Config.ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 Config.ASSET_DIR = os.path.join(Config.ROOT_DIR, 'brokenjukebox/assets')
 Config.AUDIO_ASSET_DIR = os.path.join(Config.ASSET_DIR, 'audio')
+Config.CATALOGUE_FILE = os.path.join(Config.AUDIO_ASSET_DIR,"broken_jukebox_catalogue.json")
 
 # Core logic must be imported after Config variables are set for paths etc
 from brokenjukebox.core.core import Core
-
 
 def run() -> None:
     c = Core()
